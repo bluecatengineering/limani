aee54e1c8893fe72c4e00ccde1b42c3c62568257
Edwin Christie <echristie@bluecatnetworks.com>
2023-10-20 15:19:11 -0400

    chore: implement coverage for code (ENG-50810)

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    clearMocks: true,
+    moduleNameMapper: {
+        '^react$': '@bluecateng/jest-helpers/mock-react',
+        '^react-dom$': '@bluecateng/jest-helpers/mock-react',
+        '^react/jsx-runtime$': '@bluecateng/jest-helpers/mock-react',
+        '\\.(png|jpg|gif|svg|ttf|woff2)$':
+            '@bluecateng/jest-helpers/file-mapper',
+        '\\.(less)$': '@bluecateng/jest-helpers/style-mapper',
+    },
+    setupFiles: ['<rootDir>/jest-helpers/setup-globals'],
+    setupFilesAfterEnv: ['@bluecateng/jest-helpers/setup-enzyme'],
+    transform: {
+        '\\.[jt]sx?$': 'babel-jest',
+        '\\.po$': '@bluecateng/l10n-jest',
+    },
+    transformIgnorePatterns: [
+        'node_modules/(?!(lodash-es|@bluecat|@bluecateng)/)',
+    ],
+    collectCoverage: true,
+    coverageReporters: ['text', 'html'],
+    coveragePathIgnorePatterns: ['/dist/', '/node_modules/', '/jest-helpers/'],
+    coverageThreshold: {
+        global: {
+            branches: 40,
+            functions: 29,
+            lines: 46,
+            statements: 45,
+        },
+    },
+};
