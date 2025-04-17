0a1f1dc628efebf56b36f6add071ad417b0cb466
Andrew Pham <apham@bluecatnetworks.com>
2025-04-17 09:52:51 +0700

    chore(ui): revert target and rel for authenticationLink (#2)(ENG-61949)

--- src/header/HeaderAuthentication.js
+++ src/header/HeaderAuthentication.js
@@ -45,7 +45,10 @@ const HeaderAuthentication = ({ className }) => {
                     }`}>
                     <span>
                         {authenticationAlias ? (
-                            <a href={authenticationLink}>
+                            <a
+                                target='_blank'
+                                rel='noreferrer'
+                                href={authenticationLink}>
                                 {authenticationService}:{' '}
                                 <u>{authenticationAlias}</u>
                             </a>
